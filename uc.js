b6f4bbd9d786c4925466827ffacb4014660d7fbc
preethamsetty <preethambs2002@gmail.com>
2024-09-30 22:25:03 +0530

    [Preetham B S] - added a uc10 file

--- uc.js
+++ uc.js
@@ -1,4 +1,4 @@
-// UC9: View persons by city or state
+// UC10: Get number of contact persons by city or state
 
 class AddressBook {
     constructor() {
@@ -9,19 +9,15 @@ class AddressBook {
         this.contacts.push(contact);
     }
 
-    getPersonsByCity(city) {
-        return this.contacts.filter(contact => contact.city === city);
+    getCountByCity(city) {
+        return this.contacts.filter(contact => contact.city === city).length;
     }
 
-    getPersonsByState(state) {
-        return this.contacts.filter(contact => contact.state === state);
+    getCountByState(state) {
+        return this.contacts.filter(contact => contact.state === state).length;
     }
 }
 
 // Example
 let addressBook = new AddressBook();
-addressBook.addContact(new Contact("John", "Doe", "123 Street", "CityA", "StateA", "123456", "1234567890", "john@example.com"));
-addressBook.addContact(new Contact("Jane", "Smith", "456 Avenue", "CityB", "StateA", "654321", "0987654321", "jane@example.com"));
-
-console.log("Persons in CityA: ", addressBook.getPersonsByCity("CityA"));
-console.log("Persons in StateA: ", addressBook.getPersonsByState("StateA"));
+addressBook
