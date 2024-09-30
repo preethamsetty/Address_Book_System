11cc8de9524635319a7a09e1388b95e35e0a1fbd
preethamsetty <preethambs2002@gmail.com>
2024-09-30 22:24:14 +0530

    [Preetham B S] - added a uc9 file

--- uc.js
+++ uc.js
@@ -1,4 +1,4 @@
-// UC8: Search for a person in a particular city or state
+// UC9: View persons by city or state
 
 class AddressBook {
     constructor() {
@@ -9,11 +9,11 @@ class AddressBook {
         this.contacts.push(contact);
     }
 
-    searchByCity(city) {
+    getPersonsByCity(city) {
         return this.contacts.filter(contact => contact.city === city);
     }
 
-    searchByState(state) {
+    getPersonsByState(state) {
         return this.contacts.filter(contact => contact.state === state);
     }
 }
@@ -23,5 +23,5 @@ let addressBook = new AddressBook();
 addressBook.addContact(new Contact("John", "Doe", "123 Street", "CityA", "StateA", "123456", "1234567890", "john@example.com"));
 addressBook.addContact(new Contact("Jane", "Smith", "456 Avenue", "CityB", "StateA", "654321", "0987654321", "jane@example.com"));
 
-console.log("Contacts in StateA: ", addressBook.searchByState("StateA"));
-console.log("Contacts in CityA: ", addressBook.searchByCity("CityA"));
+console.log("Persons in CityA: ", addressBook.getPersonsByCity("CityA"));
+console.log("Persons in StateA: ", addressBook.getPersonsByState("StateA"));
