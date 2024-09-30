2780b3e8546ee3cff6d4797cb608025822adbeb0
preethamsetty <preethambs2002@gmail.com>
2024-09-30 22:15:14 +0530

    [Preetham B S] - added a uc3 file

--- uc.js
+++ uc.js
@@ -1,49 +1,4 @@
-// UC2: Validation for adding contacts
-
-class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
-        if (!this.validateName(firstName) || !this.validateName(lastName)) {
-            throw new Error("Invalid name. Should start with capital and have at least 3 characters.");
-        }
-
-        if (!this.validateAddress(address) || !this.validateAddress(city) || !this.validateAddress(state)) {
-            throw new Error("Invalid address/city/state. Should have at least 4 characters.");
-        }
-
-        if (!this.validateZip(zip) || !this.validatePhone(phone) || !this.validateEmail(email)) {
-            throw new Error("Invalid Zip/Phone/Email.");
-        }
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    validateName(name) {
-        return /^[A-Z][a-zA-Z]{2,}$/.test(name);
-    }
-
-    validateAddress(address) {
-        return address.length >= 4;
-    }
-
-    validateZip(zip) {
-        return /^\d{5,6}$/.test(zip);
-    }
-
-    validatePhone(phone) {
-        return /^\d{10}$/.test(phone);
-    }
-
-    validateEmail(email) {
-        return /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email);
-    }
-}
+// UC3: Ability to create multiple Address Books and add contacts
 
 class AddressBook {
     constructor() {
@@ -59,11 +14,9 @@ class AddressBook {
     }
 }
 
-// Example with validation
-try {
-    let addressBook = new AddressBook();
-    addressBook.addContact(new Contact("John", "Doe", "123 Street", "City", "State", "123456", "1234567890", "john@example.com"));
-    console.log(addressBook.getAllContacts());
-} catch (error) {
-    console.error(error.message);
-}
+// Adding multiple contacts
+let addressBook1 = new AddressBook();
+addressBook1.addContact(new Contact("John", "Doe", "123 Street", "City", "State", "123456", "1234567890", "john@example.com"));
+addressBook1.addContact(new Contact("Jane", "Smith", "456 Avenue", "Town", "Province", "654321", "0987654321", "jane@example.com"));
+
+console.log(addressBook1.getAllContacts());
