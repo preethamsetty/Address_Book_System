7851c08c0511f046dfc8327ed33de614772abd5e
preethamsetty <preethambs2002@gmail.com>
2024-09-30 22:27:02 +0530

    [Preetham B S] - added and modified a uc10 file

--- uc.js
+++ uc.js
@@ -20,4 +20,8 @@ class AddressBook {
 
 // Example
 let addressBook = new AddressBook();
-addressBook
+addressBook.addContact(new Contact("John", "Doe", "123 Street", "CityA", "StateA", "123456", "1234567890", "john@example.com"));
+addressBook.addContact(new Contact("Jane", "Smith", "456 Avenue", "CityB", "StateA", "654321", "0987654321", "jane@example.com"));
+
+console.log("Number of persons in CityA: ", addressBook.getCountByCity("CityA"));
+console.log("Number of persons in StateA: ", addressBook.getCountByState("StateA"));
