027ba0971100300d750ad4a90e439aabcf70c151
preethamsetty <preethambs2002@gmail.com>
2024-09-30 22:22:23 +0530

    [Preetham B S] - added a uc7 file

--- uc.js
+++ uc.js
@@ -1,4 +1,4 @@
-// UC6: Ability to find the number of contacts using reduce
+// UC7: Ensure no duplicate entries based on name
 
 class AddressBook {
     constructor() {
@@ -6,21 +6,23 @@ class AddressBook {
     }
 
     addContact(contact) {
+        const isDuplicate = this.contacts.some(c => c.firstName === contact.firstName && c.lastName === contact.lastName);
+        if (isDuplicate) {
+            throw new Error("Duplicate contact detected.");
+        }
         this.contacts.push(contact);
     }
 
     getAllContacts() {
         return this.contacts;
     }
-
-    // Get number of contacts using reduce
-    getContactCount() {
-        return this.contacts.reduce(count => count + 1, 0);
-    }
 }
 
 // Example
-let addressBook = new AddressBook();
-addressBook.addContact(new Contact("John", "Doe", "123 Street", "City", "State", "123456", "1234567890", "john@example.com"));
-addressBook.addContact(new Contact("Jane", "Smith", "456 Avenue", "Town", "Province", "654321", "0987654321", "jane@example.com"));
-console.log("Number of contacts: " + addressBook.getContactCount());
+try {
+    let addressBook = new AddressBook();
+    addressBook.addContact(new Contact("John", "Doe", "123 Street", "City", "State", "123456", "1234567890", "john@example.com"));
+    addressBook.addContact(new Contact("John", "Doe", "456 Avenue", "City", "State", "654321", "0987654321", "john2@example.com"));  // Duplicate entry
+} catch (error) {
+    console.error(error.message);  // Should print "Duplicate contact detected."
+}
